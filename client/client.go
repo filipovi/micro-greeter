8d8eabedefb61bf1cbdd247344b6d7cab73ad89f
Pascal Filipovicz <pascal.filipovicz@frozenk.net>
2018-08-21 12:56:53 +0200

    add the client

---
+++ client/client.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	proto "github.com/filipovi/micro1/proto"
+	micro "github.com/micro/go-micro"
+)
+
+// Greeter is a struct containing the name
+type Greeter struct{}
+
+func main() {
+	service := micro.NewService()
+	greeter := proto.NewGreeterClient("greeter", service.Client())
+
+	// request the Hello method on the Greeter handler
+	rsp, err := greeter.Hello(context.TODO(), &proto.HelloRequest{
+		Name: "Pascal",
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(rsp.Greeting)
+}
